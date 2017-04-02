0e9642ddf62ffea279f992f5d6a52052f7bf458f
Santiago Perez <rsperezn@gmail.com>
2017-04-02 18:59:53 +1000

    Refactored to include declarative rest call, and renamed to lab-6-sentence

--- lab-5-sentence/src/main/kotlin/com/rspn/Lab6SentenceApplication.kt
+++ lab-5-sentence/src/main/kotlin/com/rspn/Lab6SentenceApplication.kt
@@ -3,11 +3,13 @@ package com.rspn
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.feign.EnableFeignClients
 
 @SpringBootApplication
 @EnableDiscoveryClient
-open class Lab4SubjectApplication
+@EnableFeignClients
+open class Lab6SentenceApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Lab4SubjectApplication::class.java, *args)
+    SpringApplication.run(Lab6SentenceApplication::class.java, *args)
 }
