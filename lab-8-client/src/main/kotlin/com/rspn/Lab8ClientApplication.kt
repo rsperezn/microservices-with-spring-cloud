dfd52e863d4badf37c39332b2fd2f9a318c48a0b
Santiago Perez <rsperezn@gmail.com>
2017-04-11 19:08:46 +1000

    Added spring cloud bus for on the fly config updates. Note the Camdem version

---
+++ lab-8-client/src/main/kotlin/com/rspn/Lab8ClientApplication.kt
@@ -0,0 +1,11 @@
+package com.rspn
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class Lab8ClientApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Lab8ClientApplication::class.java, *args)
+}
